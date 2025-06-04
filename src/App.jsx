1c7452f63f36401e2106bb8cf72e012e95add44b
baokun2115 <baotran2115@gmail.com>
2025-06-04 23:08:50 +0700

    fix: refactor UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from 'react';
 import './App.css';
-import { HomePage } from './pages/home';
+import { Loading } from './component/loading';
+import { getAllQuotes } from './services/apiQuote';
 
 function App() {
   const [activePage, setActivePage] = useState('home');
+  const [loading, setLoading] = useState(false);
+  const [quotes, setQuotes] = useState([]);
+
+  const onFetchQuotes = async () => {
+    setLoading(true);
+    const res = await getAllQuotes();
+    setLoading(false);
+    setQuotes(res);
+  };
 
   return (
     <>
@@ -26,9 +36,31 @@ function App() {
           Add a new quote
         </div>
       </div>
+      {loading && <Loading />}
 
-      <div>{activePage === 'home' && <HomePage />}</div>
+      <div>
+        {activePage === 'home' && (
+          <>
+            <div className='home'>
+              <h2>Get Quotes</h2>
+              <div className='buttons'>
+                <div className='button'>Fetch a random Quote</div>
+                <div className='button' onClick={onFetchQuotes}>
+                  Fetch all Quotes
+                </div>
+                <input type='text' />
+                <div className='button'>Fetch by Author</div>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
       <div>{activePage === 'addQuote' && <Add />}</div>
+      <div>
+        {quotes.map((quote, index) => (
+          <div key={index}>{quote.text}</div>
+        ))}
+      </div>
     </>
   );
 }
